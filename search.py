1d99a8a3ad90b92602d12b0f51638d2dfbc9ddb4
Ryan Demo <ryan.demo@me.com>
2017-03-16 21:40:35 -0400

    Fixed depth first search to work with autograder

--- search.py
+++ search.py
@@ -73,30 +73,35 @@ def tinyMazeSearch(problem):
     return  [s, s, w, s, w, w, s, w]
 
 def depthFirstSearch(problem):
-    """Search the deepest nodes in the search tree first.
+    """
+    Search the deepest nodes in the search tree first.
 
     Your search algorithm needs to return a list of actions that reaches the
     goal. Make sure to implement a graph search algorithm.
 
     To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:"""
-    
+    understand the search problem that is being passed in:
+
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    """
     s = util.Stack()
-    
-    s.push((problem.getStartState(), [], []))
-    while s.isEmpty() is False:
-        loc, directions, discovered = s.pop()
-        #print loc, directions, discovered
+    s.push((problem.getStartState(), []))
+    visited = set()
+
+    while not s.isEmpty():
+        loc, directions = s.pop()
+        visited.add(loc)
+        if problem.isGoalState(loc):
+            return directions
 
         for nextLoc, direction, distance in problem.getSuccessors(loc):
-            if not nextLoc in discovered:
-                if problem.isGoalState(nextLoc):
-                    directions.append(direction)
-                    return directions# + [direction]
-                s.push((nextLoc, directions + [direction], discovered + [loc]))
+            if not nextLoc in visited:
+                nextDirs = directions + [direction]
+                s.push((nextLoc, nextDirs))
 
     return []
-
    
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
